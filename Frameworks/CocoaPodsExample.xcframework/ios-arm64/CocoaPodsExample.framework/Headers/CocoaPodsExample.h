a2392d7e485ba230441cd204c7266f1ad1efa1d2
walkwel-technology <walkweltech@gmail.com>
2025-04-01 13:29:37 +0530

    add: xcframework update

---
+++ Frameworks/CocoaPodsExample.xcframework/ios-arm64/CocoaPodsExample.framework/Headers/CocoaPodsExample.h
@@ -0,0 +1,18 @@
+//
+//  CocoaPodsExample.h
+//  CocoaPodsExample
+//
+//  Created by Harsh on 01/04/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CocoaPodsExample.
+FOUNDATION_EXPORT double CocoaPodsExampleVersionNumber;
+
+//! Project version string for CocoaPodsExample.
+FOUNDATION_EXPORT const unsigned char CocoaPodsExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaPodsExample/PublicHeader.h>
+
+
